953ec179245235e7509569aa393fdc0d5971b03a
Fernando <fernando.eb@icloud.com>
2020-03-11 14:20:25 -0300

    init commit
    creación de proyecto en swiftUI

---
+++ eGym/eGym/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eGym
+//
+//  Created by Fernando on 11-03-20.
+//  Copyright © 2020 copycat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
